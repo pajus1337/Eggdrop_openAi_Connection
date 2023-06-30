f3aaf51ccd522448901f9c81a10e7507f9abdd2f
Pawel Juscinski aka Pajus <74528115+pajus1337@users.noreply.github.com>
2023-06-30 22:00:43 +0200

    Update aiConnection.tcl
    this is a more elaborate version, which comes very close to the so-called final version.
    
    - This time I changed the bot trigger from !q to nickbot - on IRC channels.
    - Messages sent to the bot on a private message are immediately recognised as a trigger.
    - I added additional options which allow to regionalize functions of the script.  ( such as trigger on prv, irc channel ) 
    
    The prompt as well as the final communication with openAI is done with an additional script, which in this case is written in python (however, it is possible to do your own code here) -> My own python code is available, which is only limited to localhost connection (in order to exclude misuse by third parties).
    In addition, however, it is possible to listen on 0.0.0.0 (here, however, I do not guarantee the security of the code, because the current work on the whole has not yet taken this into account). -> Maybe this will change in the future, 
    Please feel free to cooperate.

--- aiConnection.tcl
+++ aiConnection.tcl
@@ -1,41 +1,71 @@
 package require http
 package require tls
-
-set author "Pajus"
+package require json
 
 http::register https 443 [list ::tls::socket -autoservername true]
 
-bind pub - "!q" q_command
+set botNickName "SparkBuddy"
 
-proc q_command {nick uhost handle target args} {
-    set message [join $args " "]
+# Options
+set ::respond_to_private_messages 1 ;# 1: Yes, 0: No
+set ::respond_to_channel_questions 1 ;# 1: Yes, 0: No
+set ::reply_delay 12000 ;# Time delay in milliseconds before sending a reply
+
+bind pubm - "$botNickName: " q_command
+bind pubm - "*$botNickName*" q_command
+bind msgm - "*" q_command_private
+
+proc q_command_private {nick uhost handle target args} {
+    global respond_to_private_messages
+    set message $target
     set encodedMessage [::http::formatQuery "message" $message]
-
-    # Construct the URL with the encoded message parameter
-    set url "https://webhook_adress.com$encodedMessage"
-
-    # Sending GET request to the external server
-    set token [http::geturl $url]
+    
+    set url "http://127.0.0.1:5000/chat?$encodedMessage"
+    set token [http::geturl $url -headers {Accept-Encoding gzip}]
     http::wait $token
-
     set response [http::data $token]
     set status [http::status $token]
-
     http::cleanup $token
 
-    if {$status == "ok"} {
-        displayMessageChunks $response $target
-    } else {
-        putnow "PRIVMSG $target :Error with my brain connection: $status"
+    if {[string is true -strict $::respond_to_private_messages]} {
+        if {$status == "ok"} {
+            after $::reply_delay [list displayPrivateMessageChunks $response $nick]
+        } else {
+            putlog "Error with my brain connection: $status"
+        }
     }
 }
 
-proc displayMessageChunks {response target} {
-    set maxLength 400  ;# Adjust the maximum length as needed
+proc q_command {nick uhost handle target args} {
+    global respond_to_channel_questions botNickName
+    set message [join [lrange $args 0 end] " "] 
+    set message [string replace $message 0 [string length $botNickName] ""] ;
+    set message [string trim $message] ;
+
+    set encodedMessage [::http::formatQuery "message" $message]
+
+    set url "http://127.0.0.1:5000/chat?$encodedMessage"
+    set token [http::geturl $url -headers {Accept-Encoding gzip}]
+    http::wait $token
+    set response [http::data $token]
+    set status [http::status $token]
+    http::cleanup $token
+
+    if {[string is true -strict $::respond_to_channel_questions]} {
+        if {$status == "ok"} {
+            after $::reply_delay [list displayMessageChunks $response $target $nick]
+        } else {
+            putlog "Error with my brain connection: $status"
+        }
+    }
+}
+
+proc displayMessageChunks {response target nick} {
+    set maxLength 400 ;
     set responseLength [string length $response]
 
     if {$responseLength <= $maxLength} {
-        displayMessage $response $target
+        displayMessage $response $target $nick
     } else {
         set chunks [expr {ceil(double($responseLength) / $maxLength)}]
 
@@ -44,22 +74,53 @@ proc displayMessageChunks {response target} {
             set endIndex [expr {min($responseLength, ($i + 1) * $maxLength)}]
             set chunk [string range $response $startIndex $endIndex]
 
-            putnow "PRIVMSG $target :$chunk"
+            after $::reply_delay [list putnow "PRIVMSG $target :$chunk"]
+            vwait ::reply_ready
         }
     }
 }
 
-proc displayMessage {response target} {
+proc displayMessage {response target nick} {
     set cleanedResponse [regsub -all {\[Collection\]} $response {}]
-    set cleanedResponse [regsub -all {\[Your Name\]} $cleanedResponse "Your Name"]
-    putnow "PRIVMSG $target :$cleanedResponse"
+    set cleanedResponse [regsub -all {\[Your Name\]} $cleanedResponse "$nick"]
+    set cleanedResponse [string trim $cleanedResponse]
+    set reply [json::json2dict $cleanedResponse]
+    set replyText [dict get $reply "reply"]
+    set responseText [format "%s: %s" $nick $replyText]
+    putlog "Response displayed on $target: $responseText"
+    putnow "PRIVMSG $target :$responseText"
+    set ::reply_ready 1
 }
 
+proc displayPrivateMessageChunks {response nick} {
+    set maxLength 400 ;
+    set responseLength [string length $response]
 
-proc script_loaded {} {
-    global author
-    putlog "OpenAI v1.0 script loaded successfully."
-    putlog "Author: $author"
+    if {$responseLength <= $maxLength} {
+        displayPrivateMessage $response $nick
+    } else {
+        set chunks [expr {ceil(double($responseLength) / $maxLength)}]
+
+        for {set i 0} {$i < $chunks} {incr i} {
+            set startIndex [expr {$i * $maxLength}]
+            set endIndex [expr {min($responseLength, ($i + 1) * $maxLength)}]
+            set chunk [string range $response $startIndex $endIndex]
+
+            after $::reply_delay [list putnow "PRIVMSG $nick :$chunk"]
+            vwait ::reply_ready
+        }
+    }
 }
 
-register script_loaded
+proc displayPrivateMessage {response nick} {
+    set cleanedResponse [regsub -all {\[Collection\]} $response {}]
+    set cleanedResponse [regsub -all {\[Your Name\]} $cleanedResponse $nick]
+    set cleanedResponse [string trim $cleanedResponse]
+    set reply [json::json2dict $cleanedResponse]
+    set replyText [dict get $reply "reply"]
+    putlog "Response displayed on $nick: $replyText"
+    putnow "PRIVMSG $nick :$replyText"
+    set ::reply_ready 1
+}
+
+putlog "Pajus OpenAI Bot Script Loaded Correct"
